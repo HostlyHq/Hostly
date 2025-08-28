3b4889ec2a2984b32fcfec850264e3bb3a8b84f8
fran-897-uinx <privailfrancis@gmail.com>
2025-08-28 17:04:18 +0100

    upated the packages

--- src/components/packageCard.jsx
+++ src/components/packageCard.jsx
@@ -1,28 +1,42 @@
-import formatPrice from '../utils/formatPrice.jsx';
+import formatBillingPrice from "../utils/formatBillingPrice.jsx";
 
-export default function PackageCard (props) {
-   return (
-        <div key={props.header} className="flex flex-col max-w-[300px] min-w-[300px] text-gray-950
-            rounded-2xl border-2 border-solid border-gray-500
-            p-5">
-            <p className="text-[28px] font-semibold mb-2
-                text-start">{props.header}</p>
-            <p className="text-[12px] text-start mb-5">
-                {props.description}
-            </p>
-            <p className="text-5xl leading-8 font-semibold
-                text-start mb-1.5">{formatPrice(props.priceCents)}</p>
-            <p className="text-[14px] font-light text-start
-                mb-6">Per month</p>
-            <button className="w-full h-12 text-gray-950 mb-6 
-                bg-white rounded-[14px] border-2 text-[14px]
-                font-semibold border-gray-950">Choose this plan</button>
-            <div>
-                <ul className='text-start pl-5 text-gray-500 font-semibold
-                    leading-6 list-disc'>
-                    {props.benefit}
-                </ul>
-            </div>
-        </div>
-   )           
+export default function PackageCard({
+  header,
+  description,
+  priceCents,
+  benefit,
+  billingType,
+}) {
+  return (
+    <div
+      className="flex flex-col max-w-[300px] min-w-[300px] text-blue-950
+        rounded-2xl border-2 border-solid border-gray-500
+        p-5 hover:shadow-amber-950 hover:shadow-md"
+    >
+      <p className="text-[28px] font-semibold mb-2 text-start">{header}</p>
+      <p className="text-[12px] text-start mb-5">{description}</p>
+
+      <p className="text-5xl leading-8 font-semibold text-start mb-1.5">
+        {formatBillingPrice(priceCents, billingType)}
+      </p>
+
+      <p className="text-[14px] font-light text-start mb-6">
+        {billingType === "monthly" ? "Per month" : "Per year"}
+      </p>
+
+      <button
+        className="w-full h-12 text-gray-950 mb-6 
+          bg-white rounded-[14px] border-2 text-[14px]
+          font-semibold border-gray-950 hover:bg-blue-700 hover:text-gray-200"
+      >
+        Choose this plan
+      </button>
+
+      <div>
+        <ul className="text-start pl-5 text-gray-500 font-semibold leading-6 list-disc">
+          {benefit}
+        </ul>
+      </div>
+    </div>
+  );
 }
